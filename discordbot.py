09becf8076258b0d3e7c97827a3c5aaab990d8f6
ちょこばた <support@c5bt.net>
2018-08-23 04:08:38 +0900

    bug fix

--- discordbot.py
+++ discordbot.py
@@ -11,10 +11,6 @@ async def on_message(message):
         reply = 'にゃーん'
         await client.send_message(message.channel, reply)
     if message.content.startswith('/tukino'):
-        score = random.randrange(100)
         replay = 'まじきちルーレットスタート\n'
-        reply += 'ﾄﾞｺﾄﾞｺ┗(^o^)┛ﾄﾞｺﾄﾞｺ┏(^o^)┓ﾄﾞｺﾄﾞｺ┗(^o^)┛ﾄﾞｺﾄﾞｺ┏(^o^)┓ﾄﾞｺﾄﾞｺ┗(^o^)┛ﾄﾞｺﾄﾞｺ┏(^o^)┓ﾄﾞｺﾄﾞｺ┗(^o^)┛ﾄﾞｺﾄﾞｺ┏(^o^)┓ﾄﾞｺﾄﾞｺ┗(^o^)┛\n'
-        reply += str(score) + '点でした'
-        print(reply)
         await client.send_message(message.channel, reply)
 client.run('NDgwNDQ1NTEyNTEyODMxNDg5.Dlp6ow.E1_2sSoBAYM4cqZywwGZZNl2EkM')
