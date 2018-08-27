9fd962d518acae134a495b9f79b756bfdeb38cb2
ちょこばた <support@c5bt.net>
2018-08-27 18:15:57 +0900

    fix omikuji bug3

--- discordbot.py
+++ discordbot.py
@@ -17,17 +17,18 @@ async def on_message(message):
     elif message.content.startswith('/tukino'):
         msg = ['マジキチルーレットスタート:rolling_eyes:','ドコドコ┗(^o^)┛','今日の運勢は:question:']
         result = ['大大吉','大吉','凶後大吉','凶後吉','末大吉','末吉','向大吉','吉','中吉','小吉','小吉後吉','後吉','吉凶末分末大吉','吉凶不分末吉','吉凶相半','吉凶相交末吉','吉凶相央']
-        rand = random.randrange(0,17)
+        rand = randint(17)
         await client.send_message(message.channel, msg[0])
         sleep(1)
-        for num in range(5):
+        num = 0
+        while num < 5:
             await client.send_message(message.channel, msg[1])
+            num += 1
             sleep(0.2)
-            break
         sleep(1)
         await client.send_message(message.channel, msg[2])
         sleep(2)
-        await clinet.send_message(message.channel,result[rand])
+        await clinet.send_message(message.channel, result[rand])
 
 
 
