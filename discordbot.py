3a2911f837c007ed616b13900112f10da38e435f
ちょこばた <support@c5bt.net>
2018-08-29 11:46:41 +0900

    fix command bug

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 from time import sleep
 
-prefix = 'c!'//コマンド接頭辞
+prefix = 'c!' #コマンド接頭語
 
 client = discord.Client()
 bot = commands.Bot(command_prefix = prefix)
